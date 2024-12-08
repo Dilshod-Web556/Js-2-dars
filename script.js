83825ba69d0905e9fdcedd5fad5531eaa48b2f06
Dilshod-Web556 <GorillaEnergy777@Gmail.com>
2024-12-08 15:12:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let name = prompt('String typedagi ma`lumot kiriting');
+
+let num = +prompt('Number typedagi ma`lumot kiriting');
+
+let age = prompt('Boolean typedagi ma`lumot kiriting');
+
+let main = ('Hamma ma`lumotlarni to`g`ri kiritganizga ishonhingiz komilmi?');
+alert(main)
+
+let seting = (`Siz kiritgan ${name} sozi string typega tegishli
+ Siz kiritgan ${num} sozi number typega tegishli
+ Siz kiritgan ${age} sozi boolean typega tegishli`);
+
+    alert(seting);
